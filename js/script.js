5436fffaaf36f6b2ad8726110829538a75ab33f4
rebs-code <jack.rebussi@gmail.com>
2023-11-27 16:31:56 +0100

    feat: prev and next button on thumbnail

--- js/script.js
+++ js/script.js
@@ -33,28 +33,45 @@ createApp({
     return {
       games: {
         image: [
-            "img/01.webp",
-            "img/02.webp",
-            "img/03.webp",
-            "img/04.webp",
-            "img/05.webp"
+          "img/01.webp",
+          "img/02.webp",
+          "img/03.webp",
+          "img/04.webp",
+          "img/05.webp",
         ],
         title: [
-            "Marvel's Spiderman Miles Morale",
-            "Ratchet & Clank: Rift Apart",
-            "Fortnite",
-            "Stray",
-            "Marvel's Avengers"
+          "Marvel's Spiderman Miles Morale",
+          "Ratchet & Clank: Rift Apart",
+          "Fortnite",
+          "Stray",
+          "Marvel's Avengers",
         ],
         text: [
-            "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
-            "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
-            "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-            "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
-            "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-        ]
+          "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+          "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+          "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+          "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
+          "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+        ],
       },
       currentIndex: 0,
+
     };
+  },
+  methods: {
+    nextGame() {
+      if (this.currentIndex < this.games.image.length - 1) {
+        this.currentIndex++;
+      } else {
+        this.currentIndex = 0;
+      }
+    },
+    previousGame() {
+      if (this.currentIndex > 0) {
+        this.currentIndex--;
+      } else {
+        this.currentIndex = this.games.image.length - 1;
+      }
+    },
   },
 }).mount("#app");
