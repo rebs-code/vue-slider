454de274ad5dce68af38874976bf04d1178261f1
rebs-code <jack.rebussi@gmail.com>
2023-11-27 18:03:01 +0100

    feat: refactored data

--- js/script.js
+++ js/script.js
@@ -1,66 +1,44 @@
-// const slides = [
-//   {
-//     image: "img/01.webp",
-//     title: "Marvel's Spiderman Miles Morale",
-//     text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
-//   },
-//   {
-//     image: "img/02.webp",
-//     title: "Ratchet & Clank: Rift Apart",
-//     text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
-//   },
-//   {
-//     image: "img/03.webp",
-//     title: "Fortnite",
-//     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-//   },
-//   {
-//     image: "img/04.webp",
-//     title: "Stray",
-//     text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
-//   },
-//   {
-//     image: "img/05.webp",
-//     title: "Marvel's Avengers",
-//     text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-//   },
-// ];
+"use strict";
 
 const { createApp } = Vue;
 
 createApp({
-  data() {
+data() {
     return {
-      games: {
-        image: [
-          "img/01.webp",
-          "img/02.webp",
-          "img/03.webp",
-          "img/04.webp",
-          "img/05.webp",
+        games: [
+            {
+                image: "img/01.webp",
+                title: "Marvel's Spiderman Miles Morale",
+                text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man."
+            },
+            {
+                image: "img/02.webp",
+                title: "Ratchet & Clank: Rift Apart",
+                text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality."
+            },
+            {
+                image: "img/03.webp",
+                title: "Fortnite",
+                text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100-player face-off that combines looting, crafting, shootouts and chaos."
+            },
+            {
+                image: "img/04.webp",
+                title: "Stray",
+                text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city."
+            },
+            {
+                image: "img/05.webp",
+                title: "Marvel's Avengers",
+                text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay."
+            }
         ],
-        title: [
-          "Marvel's Spiderman Miles Morale",
-          "Ratchet & Clank: Rift Apart",
-          "Fortnite",
-          "Stray",
-          "Marvel's Avengers",
-        ],
-        text: [
-          "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
-          "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
-          "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-          "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
-          "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
-        ],
-      },
-      currentIndex: 0,
-
+        currentIndex: 0,
+        //variabile time ID
     };
-  },
+},
   methods: {
     nextGame() {
-      if (this.currentIndex < this.games.image.length - 1) {
+      if (this.currentIndex < this.games.length - 1) {
         this.currentIndex++;
       } else {
         this.currentIndex = 0;
@@ -70,7 +48,7 @@ createApp({
       if (this.currentIndex > 0) {
         this.currentIndex--;
       } else {
-        this.currentIndex = this.games.image.length - 1;
+        this.currentIndex = this.games.length - 1;
       }
     },
   },
